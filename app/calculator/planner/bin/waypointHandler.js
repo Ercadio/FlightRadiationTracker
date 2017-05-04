9ea4da2f12e093395877ede92d3413802122793e
Ercadio <vroch@edu.uwaterloo.ca>
2017-05-04 11:57:26 -0400

    modified:   app/calculator/planner/bin/waypointHandler.js 	modified:   app/calculator/planner/index.html

--- app/calculator/planner/bin/waypointHandler.js
+++ app/calculator/planner/bin/waypointHandler.js
@@ -17,7 +17,7 @@ window.addEventListener('load', function() {
       obj = document.createElement('div');
       obj.className = 'pointlist';
       obj.style.display = 'none';
-      obj.innerHTML = 'lat<input type=\'text\' class=\'coor\' id=\'lat' + counter + '\'/>     lon<input type=\'text\' class=\'coor\' id=\'lon' + counter + '\'/><br\>uptime<input class=\'coor\' id=\'time' + counter + '\'/>     alt<input type=\'text\' class=\'coor\' id=\'alt' + counter + '\'/><br\>';
+      obj.innerHTML = 'lat(deg)<input type=\'text\' class=\'coor\' id=\'lat' + counter + '\'/>              lon(deg)<input type=\'text\' class=\'coor\' id=\'lon' + counter + '\'/>‌<br\>uptime (min)<input class=\'coor\' id=\'time' + counter + '\'/>     alt (ft)<input type=\'text\' class=\'coor\' id=\'alt' + counter + '\'/><br\>';
       obj.id = 'point' + counter;
       waypointList.push(obj);
       container.appendChild(obj);
